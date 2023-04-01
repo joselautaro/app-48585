db9f6f527979435d49ab0c05745611e03b2841c4
Lautaro Montaña <joselautarom@gmail.com>
2023-04-01 14:36:26 -0300

    Apicamos busqueda y peticiones segun nombre y id

--- src/components/Pika/Pika.jsx
+++ src/components/Pika/Pika.jsx
@@ -20,21 +20,64 @@ export const Pika = () =>{
                     img: data.sprites.front_default,
                 })
             })
-    }, [])
+    }, [id])
 
 
+    const anterior = ( ) =>{
+        id > 1 && setId (id -1)
+    } 
 
+    const siguiente = () =>{
+        setId(id +1)
+    }
+
+    const inputChange = (e) =>{
+        setBusqueda(e.target.value)
+    }
+    const submit = (e)=>{
+        e.preventDefault()
+        if(busqueda.length >2){
+            fetch(`https://pokeapi.co/api/v2/pokemon/${busqueda}`)
+            .then((res) => res.json())
+            .then((data) =>{
+                setPokemon({
+                    nombre: data.name,
+                    img: data.sprites.front_default,
+                })
+                setId(
+                    data.id
+                )
+            })
+        }
+    }
 
 
 
 
     return(
         <>
+        <div>
+        {
+        !pokemon ? <h3>Cargando...</h3>:
+        <>
         <h3 className='pika'>{pokemon.numero}</h3>
         <h3 className='pika'>{pokemon.nombre}</h3>
-        <img src={pokemon.img} />
+        <img src={pokemon.img}/>
         </>
+        }
+        </div>
+        <div>
+        <button onClick={anterior}>Anterior</button>
+        <button onClick={siguiente}>Siguiente</button>
+        </div>
         
+        <>
+        <form onSubmit={submit}>
+        <input type="text" value={busqueda} onChange={inputChange}/>
+        </form>
+            
+        </>
+        </>
         
     )
 }
