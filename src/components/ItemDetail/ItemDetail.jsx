82a69e29e10a35d76f23bc8f073802262d933fdd
Lautaro Montaña <joselautarom@gmail.com>
2023-04-15 13:57:05 -0300

    Agregamos el counter en la vista de detalle

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap'
+import { ItemCount } from '../ItemCount/ItemCount'
 
 export const ItemDetail = ({ id, description, price, image, category }) => {
   // Pasamos mediante props, cada propiedad de nuestro productos(objetos) y lo colocamos por props en nuestro return
   return (
     <div className='item'>
-      <Card style={{ width: '19rem' }}>
+      <Card style={{ width: '25rem' }}>
         <Card.Img variant="top" src={image} />
         <Card.Body>
           <Card.Title>{id}</Card.Title>
           <Card.Title>{description}</Card.Title>
           <Card.Title>{price}</Card.Title>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aut tempora perspiciatis soluta sint nihil error deserunt at, nemo quas nobis. Ducimus, mollitia accusantium autem quia ullam tempore sint laboriosam?</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea error totam quibusdam doloribus, alias aperiam exercitationem laboriosam illum similique eos, delectus vitae odit maxime, repellendus iusto quisquam placeat blanditiis. Cupiditate! </p>
           <Card.Title>Categoria: {category}</Card.Title>
-            <Button variant="primary">Agregar al carrito</Button>
+          <ItemCount />
+          <Button variant="primary">Agregar al carrito</Button>
         </Card.Body>
       </Card>
     </div>
