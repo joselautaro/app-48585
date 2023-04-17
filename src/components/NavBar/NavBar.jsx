6d265295567c1349e284b70181cbefea6ad4b7c2
Lautaro Montaña <joselautarom@gmail.com>
2023-04-17 12:51:59 -0300

    Cambiamos estilos en el NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,13 +7,12 @@ export const NavBar = () => {
     return (
         <div className="menu">
             {/* <Link to='/'>Logo</Link> */}
-            <Link to="/">Inicio</Link>
-            <Link to='/productos/electrodomestico'>Electrodomesticos</Link>
-            <Link to='/productos/ropa'>Ropa</Link>
+            <Link className='link' to="/">Inicio</Link>
+            <Link className='link' to='/productos/electrodomestico'>Electrodomesticos</Link>
+            <Link className='link' to='/productos/ropa'>Ropa</Link>
             {/* <Link to="/counter">Contador</Link>
-            <Link to="/pika">Pokemons</Link> */}
-            <Link to='/cart'><CartWidget/></Link>
-            
+            <Link className='link' to="/pika">Pokemons</Link> */}
+            <Link className='link' to='/cart'><CartWidget/></Link>
         </div>
     )
 }
