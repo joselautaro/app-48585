301fc12f48b253dd747d5ef81f4534a180bd2fef
Lautaro Montaña <joselautarom@gmail.com>
2023-04-22 12:42:29 -0300

    Completamos vista

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer"
+import {CartScreen} from './components/CartScreen/CartScreen'
 import { NavBar } from "./components/NavBar/NavBar";
 import { ItemCount } from './components/ItemCount/ItemCount';
 import { Pika } from './components/Pika/Pika';
@@ -27,10 +28,24 @@ function App() {
     return carrito.reduce((acc, prod) => acc + prod.counter, 0)
   }
 
+  const precioTotal = () =>{
+    return carrito.reduce((acc, prod) => acc + prod.price * prod.counter, 0)
+  }
+
+  const removerItem = (itemId) =>{
+      const newCart = carrito.filter((prod) => prod.id !== itemId)
+      setCarrito(newCart)
+  }
+
+
+
   return (
     <CartContext.Provider value={{
       addToCart,
-      calcularCantidad
+      calcularCantidad,
+      precioTotal,
+      removerItem,
+      carrito
     }}>
       <div className='App'>
         <Router>
@@ -40,6 +55,7 @@ function App() {
             <Route path='/productos/:categoryId' element={<ItemListContainer />} />
             <Route path='/detail/:itemId' element={<ItemDetailContainer />} />
             <Route path='/counter' element={<ItemCount />} />
+            <Route path='/cart' element={<CartScreen/>}/>
             <Route path='/pika' element={<Pika />} />
             <Route path='*' element={<Navigate to='/' />} />
           </Routes>
--- src/components/CartScreen/CartScreen.jsx
+++ src/components/CartScreen/CartScreen.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import { BsFillTrashFill } from "react-icons/bs";
+import { CartContext } from '../context/CartContext';
 
 export const CartScreen = () => {
+
+  const {carrito, precioTotal, removerItem} = useContext(CartContext)
+
+
   return (
-    <div className='container my-5'>
-        <h3>Resumen de compras</h3>
-        <hr />
-        <div className="listado">
-                <p>Producto</p>
-                <p>$</p>
-                <p>cantidad: </p>
-                <Button className='btn btn-danger'>
-                    <BsFillTrashFill/>
-                </Button>
+    <div className='container'>
+    {
+      carrito.map((prod) =>(
+        <>
+        <div className='listado'>
+          <p>Producto: {prod.description}</p>
+          <p>${prod.price}</p>
+          <p>cantidad: {prod.counter}</p>
         </div>
-
-
+        <Button onClick={() => removerItem (prod.id)}>
+          <BsFillTrashFill/>
+        </Button>
+        </>
+      ))
+    }
+    <hr />
+    <strong>Precio total: $ {precioTotal()}</strong>
     </div>
   )
 }
