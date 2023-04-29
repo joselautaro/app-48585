4d7dbdab1d9f95a6537e3c17321fee6e07d3e6e7
Lautaro Montaña <joselautarom@gmail.com>
2023-04-29 14:32:12 -0300

    Creamos orden de compra, actualizamos en la base de datos y hacemos una notificación de compra realizada con éxito

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import { CartContext } from '../context/CartContext'
 import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import { getFirestore } from '../../firebase/config'
+import firebase from "firebase"
+import 'firebase/firestore'
 
 export const Checkout = () => {
+
+  const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext)
 
 
   const [email, setEmail] = useState("")
 
-  const [nombre, setNombre ] = useState("")
+  const [nombre, setNombre] = useState("")
 
   const [apellido, setApellido] = useState("")
 
   const [telefono, setTelefono] = useState("")
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
 
     e.preventDefault()
 
@@ -21,37 +28,81 @@ export const Checkout = () => {
     console.log("Apellido:", apellido)
     console.log("Teléfono:", telefono)
 
-  }
+    const orden = {
+      buyer: {
+        email,
+        nombre,
+        apellido,
+        telefono
+      },
+      item: carrito,
+      total_price: precioTotal(),
+      data: firebase.firestore.Timestamp.fromDate(new Date())
+    }
+    console.log(orden)
 
+    // Hacemos funcion para enviar la orden a firebase
+    const db = getFirestore()
+
+    const ordenes = db.collection('ordenes')
+
+    ordenes.add(orden)
+      .then((res) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Su compra fue realizada con éxitos',
+          text: `Guarde su numero de compra: ${res.id}`,
+          willClose: () => {
+            vaciarCarrito()
+          }
+        })
+      })
+      .finally(() => {
+        console.log('Operacion realizada con exito')
+      })
+
+    // Actualizamos la cantidad en la base de datos
+
+    carrito.forEach((item) => {
+      const docRef = db.collection('productos').doc(item.id)
+
+      docRef.get()
+        .then((doc) => {
+          docRef.update({
+            stock: doc.data().stock - item.counter
+          })
+        })
+    })
+  }
 
 
   return (
     <div>
 
-        <h3>Terminar compra</h3>
-        <hr />
+      <h3>Terminar compra</h3>
+      <hr />
 
-        <form onSubmit={handleSubmit} className='container mt-3'>
+      <form onSubmit={handleSubmit} className='container mt-3'>
 
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-              <input type="text" className="form-control" onChange={(e) => setEmail(e.target.value)} value={email}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="nombre">Nombre</label>
-              <input type="text" className="form-control" onChange={(e) => setNombre(e.target.value)} value={nombre}/>
-            </div>
-          <div className="form-group">
-            <label htmlFor="apellido">Apellido</label>
-              <input type="text" className="form-control" onChange={(e) => setApellido(e.target.value)} value={apellido}/>
-            </div>
-          <div className="form-group">
-            <label htmlFor="telefono">Teléfono</label>
-              <input type="text" className="form-control" onChange={(e) => setTelefono(e.target.value)} value={telefono}/>
-            </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="text" className="form-control" onChange={(e) => setEmail(e.target.value)} value={email} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre</label>
+          <input type="text" className="form-control" onChange={(e) => setNombre(e.target.value)} value={nombre} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="apellido">Apellido</label>
+          <input type="text" className="form-control" onChange={(e) => setApellido(e.target.value)} value={apellido} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="telefono">Teléfono</label>
+          <input type="text" className="form-control" onChange={(e) => setTelefono(e.target.value)} value={telefono} />
+        </div>
         <button type='submit' className='btn btn-success'>Finalizar</button>
         <Link to='/cart' className='btn btn-info'>Volver al carrito</Link>
-        </form> 
+      </form>
 
     </div>
   )
