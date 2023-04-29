46c64fd959fbac6cd630edba90b4ce5977045c9f
Lautaro Montaña <joselautarom@gmail.com>
2023-04-29 13:14:34 -0300

    Agregamos filtrado entre categorias

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -39,19 +39,34 @@ export const ItemListContainer = () => {
 
     const productos = db.collection('productos')
 
-    productos.get()
+    if(categoryId){
+      const filtrado = productos.where("category", "==", categoryId)
+      filtrado.get()
           .then((res) =>{
             const newItem = res.docs.map((doc) =>{
-              return {id: doc.id, ...doc.data()}
+              return{id: doc.id, ...doc.data()}
             })
-            console.table(newItem)
             setItems(newItem)
           })
-          .catch((error) => console.log(error))
+          .catch((err) => console.log(err))
           .finally(() =>{
             setLoading(false)
           })
 
+    }else{
+      productos.get()
+      .then((res) =>{
+        const newItem = res.docs.map((doc) =>{
+          return {id: doc.id, ...doc.data()}
+        })
+        console.table(newItem)
+        setItems(newItem)
+      })
+      .catch((error) => console.log(error))
+      .finally(() =>{
+        setLoading(false)
+      })
+    }
   }, [categoryId])
 
 
